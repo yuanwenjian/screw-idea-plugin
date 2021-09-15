58cc7c427b9c5af6cdb3aaf2851180829886f9a5
yuanwenjian <yuanwj0929@163.com>
2021-09-15 16:26:36 +0800

    screw版本203

--- src/main/java/cn/screw/idea/notify/ScrewNotifier.java
+++ src/main/java/cn/screw/idea/notify/ScrewNotifier.java
@@ -1,16 +1,18 @@
 package cn.screw.idea.notify;
 
-
-import com.intellij.notification.NotificationGroupManager;
+import com.intellij.notification.NotificationDisplayType;
+import com.intellij.notification.NotificationGroup;
 import com.intellij.notification.NotificationType;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.Nullable;
 
 public class ScrewNotifier {
 
+    private static final NotificationGroup NOTIFICATION_GROUP =
+            new NotificationGroup("Custom Notification Group", NotificationDisplayType.BALLOON, true);
+
     public static void notifyError(@Nullable Project project, String content) {
-        NotificationGroupManager.getInstance().getNotificationGroup("Custom Notification Group")
-                .createNotification(content, NotificationType.ERROR)
+        NOTIFICATION_GROUP.createNotification(content, NotificationType.ERROR)
                 .notify(project);
     }
 }
